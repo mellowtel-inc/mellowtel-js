5f4ad41b3f3fd9b7e1135dfecc9ec3771adbb2ee
olostep <157219382+mellowtel@users.noreply.github.com>
2024-03-10 20:52:20 -0700

    feat: fix top-level await
    Version 1.1.5

--- src/pdf/pdf-getter.ts
+++ src/pdf/pdf-getter.ts
@@ -1,30 +1,39 @@
-import * as PDFJS from "pdfjs-dist/build/pdf.mjs";
-import * as PDFJSWorker from "pdfjs-dist/build/pdf.worker.mjs";
+let PDFJS: any;
+let PDFJSWorker: any;
+
+async function loadPdfJsLib() {
+  if (!PDFJS) {
+    PDFJS = await import('pdfjs-dist/build/pdf.mjs');
+    PDFJSWorker = await import('pdfjs-dist/build/pdf.worker.mjs');
+  }
+  return { PDFJS, PDFJSWorker };
+}
 import { Logger } from "../logger/logger";
 export function extractTextFromPDF(pdfUrl: string): Promise<string> {
   return new Promise(async (resolve, reject) => {
+    const { PDFJS, PDFJSWorker } = await loadPdfJsLib();
     PDFJS.GlobalWorkerOptions.workerSrc = PDFJSWorker;
     PDFJS.getDocument(pdfUrl)
-      .promise.then(function (pdf: any) {
-        const textArray: string[] = [];
-        function processPage(pageNum: number) {
-          if (pageNum > pdf.numPages) {
-            let stringToResolve = "# Page 1\n" + textArray[0] + "\n";
-            for (let i = 1; i < textArray.length; i++) {
-              stringToResolve +=
+        .promise.then(function (pdf: any) {
+      const textArray: string[] = [];
+      function processPage(pageNum: number) {
+        if (pageNum > pdf.numPages) {
+          let stringToResolve = "# Page 1\n" + textArray[0] + "\n";
+          for (let i = 1; i < textArray.length; i++) {
+            stringToResolve +=
                 "# Page " + (i + 1) + "\n" + textArray[i] + "\n";
-            }
-            resolve(stringToResolve);
-            return;
           }
-          pdf
+          resolve(stringToResolve);
+          return;
+        }
+        pdf
             .getPage(pageNum)
             .then(function (page: any) {
               return page.getTextContent();
             })
             .then(function (textContent: { items: { str: string }[] }) {
               textArray.push(
-                textContent.items.map((item) => item.str).join(" "),
+                  textContent.items.map((item) => item.str).join(" "),
               );
               processPage(pageNum + 1);
             })
@@ -32,9 +41,9 @@ export function extractTextFromPDF(pdfUrl: string): Promise<string> {
               Logger.log("[extractTextFromPDF] : Error " + error);
               resolve("");
             });
-        }
-        processPage(1);
-      })
-      .catch(reject);
+      }
+      processPage(1);
+    })
+        .catch(reject);
   });
 }
