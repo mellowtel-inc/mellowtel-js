219589a269cbe651ac6b404330c92a4cbe60e9a0
yacine-bens <82149396+yacine-bens@users.noreply.github.com>
2024-07-29 14:01:49 +0100

    Added browser detection

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -24,3 +24,22 @@ export function openPopupWindow(
     resolve(true);
   });
 }
+
+export function detectBrowser() {
+  var userAgent = navigator.userAgent;
+  if (userAgent.indexOf("Edg") > -1) {
+    return "edge";
+  } else if (userAgent.indexOf("Chrome") > -1) {
+    return "chrome";
+  } else if (userAgent.indexOf("Firefox") > -1) {
+    return "firefox";
+  } else if (userAgent.indexOf("Safari") > -1) {
+    return "safari";
+  } else if (userAgent.indexOf("Opera") > -1) {
+    return "opera";
+  } else if (userAgent.indexOf("Trident") > -1 || userAgent.indexOf("MSIE") > -1) {
+    return "ie";
+  }
+
+  return "unknown";
+}
