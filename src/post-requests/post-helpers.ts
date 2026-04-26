f9772569f7f328ff0062890f0c206e4323b0efaf
mellowtel-moeez <moeezahmad024@gmail.com>
2026-04-26 23:21:39 +0500

    hotfix(dnr): scope XHR header-rewrite rule to scrape host and guarantee cleanup

--- src/post-requests/post-helpers.ts
+++ src/post-requests/post-helpers.ts
@@ -1,7 +1,7 @@
 import { Logger } from "../logger/logger";
 import {
-  disableHeadersForPOST,
-  enableHeadersForPOST,
+  disableHeadersForXHR,
+  enableHeadersForXHR,
 } from "../dnr/dnr-helpers";
 import { sendMessageToContentScript } from "../utils/messaging-helpers";
 import {
@@ -35,105 +35,104 @@ export function handlePostRequest(
   refPolicy: string = "",
 ) {
   return new Promise(async function (res) {
-    await disableHeadersForPOST();
-    // make a fetch/post to the endpoint with the payload (if not empty)
-    // then save the JSON response to the server
-    // and return the response to the caller
-    const requestOptions: {
-      method: string;
-      credentials: RequestCredentials;
-      body?: any;
-      headers?: any;
-    } = {
-      method: "POST",
-      // we're omitting credentials to avoid leaking cookies & session data
-      // this is a security measure to protect the user's data
-      credentials: "omit",
-    };
-    if (method_payload !== "no_payload") {
-      try {
-        method_payload = JSON.parse(method_payload);
-        requestOptions["body"] = JSON.stringify(method_payload);
-      } catch (e) {}
-    }
-    if (method_headers !== "no_headers") {
-      try {
-        method_headers = JSON.parse(method_headers);
-        requestOptions["headers"] = method_headers;
-      } catch (e) {}
-    }
-    let statusCode: number = 1000;
-    fetch(method_endpoint, requestOptions)
-      .then((response) => {
-        statusCode = response.status;
-        return response.text();
-      })
-      .then(async (html_or_json: string) => {
-        // Logger.log("HTML or JSON:", html_or_json);
+    await disableHeadersForXHR(method_endpoint);
+    try {
+      const requestOptions: {
+        method: string;
+        credentials: RequestCredentials;
+        body?: any;
+        headers?: any;
+      } = {
+        method: "POST",
+        // we're omitting credentials to avoid leaking cookies & session data
+        // this is a security measure to protect the user's data
+        credentials: "omit",
+      };
+      if (method_payload !== "no_payload") {
         try {
-          JSON.parse(html_or_json);
-          await saveJSON(
-            recordID,
-            JSON.parse(html_or_json),
-            orgId,
-            fastLane,
-            method_endpoint,
-            BATCH_execution,
-            batch_id,
-            statusCode,
-          );
-          await tellToDeleteIframe(
-            recordID,
-            BATCH_execution,
-            delayBetweenExecutions,
-          );
-          res(html_or_json);
-        } catch (_) {
-          Logger.log("[handlePostRequest]: Not JSON");
-          await addToRequestInfoStorage({
-            recordID: recordID,
-            isPDF: false,
-            statusCode: statusCode,
-          });
-          // not json
-          // query a tab and send a message
-          // then save the message to the server
-          chrome.tabs.query({}, async function (tabs) {
-            for (let i = 0; i < tabs.length; i++) {
-              let response = await sendMessageToContentScript(tabs[i].id!, {
-                intent: "processCrawl",
-                recordID: recordID,
-                fastLane: fastLane,
-                orgId: orgId,
-                htmlVisualizer: htmlVisualizer,
-                htmlContained: htmlContained,
-                html_string: html_or_json,
-                method_endpoint: method_endpoint,
-                removeImages: removeImages,
-                removeCSSselectors: removeCSSselectors,
-                classNamesToBeRemoved: classNamesToBeRemoved,
-                htmlTransformer: htmlTransformer,
-                BATCH_execution: BATCH_execution,
-                batch_id: batch_id,
-                statusCode: statusCode,
-                actions: actions,
-                openTab: openTab,
-                openTabOnlyIfMust: openTabOnlyIfMust,
-                saveHtml: saveHtml,
-                saveMarkdown: saveMarkdown,
-                cerealObject: cerealObject,
-              });
-              if (response !== null) {
-                break;
-              }
+          method_payload = JSON.parse(method_payload);
+          requestOptions["body"] = JSON.stringify(method_payload);
+        } catch (e) {}
+      }
+      if (method_headers !== "no_headers") {
+        try {
+          method_headers = JSON.parse(method_headers);
+          requestOptions["headers"] = method_headers;
+        } catch (e) {}
+      }
+      let statusCode: number = 1000;
+      const response = await fetch(method_endpoint, requestOptions);
+      statusCode = response.status;
+      const html_or_json = await response.text();
+
+      let isJson = false;
+      try {
+        JSON.parse(html_or_json);
+        isJson = true;
+      } catch (_) {}
+
+      if (isJson) {
+        await saveJSON(
+          recordID,
+          JSON.parse(html_or_json),
+          orgId,
+          fastLane,
+          method_endpoint,
+          BATCH_execution,
+          batch_id,
+          statusCode,
+        );
+        await tellToDeleteIframe(
+          recordID,
+          BATCH_execution,
+          delayBetweenExecutions,
+        );
+        res(html_or_json);
+      } else {
+        Logger.log("[handlePostRequest]: Not JSON");
+        await addToRequestInfoStorage({
+          recordID: recordID,
+          isPDF: false,
+          statusCode: statusCode,
+        });
+        chrome.tabs.query({}, async function (tabs) {
+          for (let i = 0; i < tabs.length; i++) {
+            let response = await sendMessageToContentScript(tabs[i].id!, {
+              intent: "processCrawl",
+              recordID: recordID,
+              fastLane: fastLane,
+              orgId: orgId,
+              htmlVisualizer: htmlVisualizer,
+              htmlContained: htmlContained,
+              html_string: html_or_json,
+              method_endpoint: method_endpoint,
+              removeImages: removeImages,
+              removeCSSselectors: removeCSSselectors,
+              classNamesToBeRemoved: classNamesToBeRemoved,
+              htmlTransformer: htmlTransformer,
+              BATCH_execution: BATCH_execution,
+              batch_id: batch_id,
+              statusCode: statusCode,
+              actions: actions,
+              openTab: openTab,
+              openTabOnlyIfMust: openTabOnlyIfMust,
+              saveHtml: saveHtml,
+              saveMarkdown: saveMarkdown,
+              cerealObject: cerealObject,
+            });
+            if (response !== null) {
+              break;
             }
-          });
-        }
-      })
-      .catch(async (error) => {
-        await enableHeadersForPOST();
-        Logger.log("Error:", error);
-      });
+          }
+        });
+        res(html_or_json);
+      }
+    } catch (error) {
+      Logger.log("Error:", error);
+      res(undefined);
+    } finally {
+      await enableHeadersForXHR(method_endpoint);
+    }
   });
 }
 
@@ -171,25 +170,24 @@ export async function saveJSON(
       };
       Logger.log("[saveJSON] : Request options => ");
       Logger.log(requestOptions);
+      // DNR cleanup is owned by the outer handlePost/handleGet try/finally.
+      // Do not call enableHeadersForPOST here: this fetch is fire-and-forget
+      // and we no longer have a URL to derive the per-host rule id from.
       fetch(targetUrl, requestOptions)
         .then(async (response) => {
           if (!response.ok) {
-            await enableHeadersForPOST();
             throw new Error("Network response was not ok");
           }
           return response.json();
         })
-        .then(async (data) => {
+        .then((data) => {
           Logger.log("[saveJSON]: Response from server:", data);
-          await enableHeadersForPOST();
         })
-        .catch(async (error) => {
+        .catch((error) => {
           Logger.log("[saveJSON] : Error:", error);
-          await enableHeadersForPOST();
         });
     } catch (e) {
       Logger.error("[saveJSON] : Error:", e);
-      await enableHeadersForPOST();
     }
   });
 }
